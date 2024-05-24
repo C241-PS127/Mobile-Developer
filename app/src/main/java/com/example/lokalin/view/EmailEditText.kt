9f8bf5db1c9ea2576b65b3630e77a373ea0381be
macbookair <rezqiseptiandri@gmail.com>
2024-05-24 18:20:26 +0700

    scrollview search Commit

---
+++ app/src/main/java/com/example/lokalin/view/EmailEditText.kt
@@ -0,0 +1,48 @@
+package com.example.lokalin.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+
+class EmailEditText @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : AppCompatEditText(context, attrs), View.OnTouchListener {
+
+
+    init {
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                val emailPattern = Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")
+                if (!s.matches(emailPattern)) {
+                    setError("Masukkan Format Email Dengan Benar", null)
+                } else {
+                    error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        hint = "Masukkan Email"
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+
+
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+
+        return false
+    }
+}
