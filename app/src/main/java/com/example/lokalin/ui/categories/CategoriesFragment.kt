fd5c0e8dfa00479fc9f047c63075c9f66af09f19
zildanpandarusm <zildanmarginata@gmail.com>
2024-05-24 23:47:26 +0700

    Add categories

--- app/src/main/java/com/example/lokalin/ui/categories/CategoriesFragment.kt
+++ app/src/main/java/com/example/lokalin/ui/categories/CategoriesFragment.kt
@@ -31,10 +31,10 @@ class CategoriesFragment : Fragment() {
         _binding = FragmentCategoriesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.tvCategories
-        categoriesViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.tvCategories
+//        categoriesViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
